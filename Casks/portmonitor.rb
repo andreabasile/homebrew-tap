955190f120e44082261912948c4aa6d1f24ea8d5
Andrea Basile <andreabasile@users.noreply.github.com>
2025-12-02 14:51:55 +0000

    fix sha

--- Casks/portmonitor.rb
+++ Casks/portmonitor.rb
@@ -1,8 +1,9 @@
 cask "portmonitor" do
     version "1.0.0"
-    sha256 "00a0ac8c24206a0a4d6283b162b7d5df8bafd9a02b24d419e34b483608cc6d86"
+    sha256 "0ee694bf0d39758f21823cb0960cc683894b942e34629525886419fa2cef9d53"
 
     url "https://github.com/andreabasile/showPorts/releases/download/v#{version}/PortMonitor-#{version}.dmg"
+         https://github.com/andreabasile/showPorts/releases/download/v1.0.0/PortMonitor-1.0.0.dmg
     name "Port Monitor"
     desc "Modern network port monitoring application for macOS"
     homepage "https://github.com/andreabasile/showPorts"
